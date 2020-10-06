6529bd16dbb491a380448facb5da242bb1bad18a
cmanage1 <55030581+cmanage1@users.noreply.github.com>
2020-10-06 15:55:51 -0400

    Update finish_task.js

--- finish_task.js
+++ finish_task.js
@@ -1,13 +1,1 @@
-//Add the task here
-function task_add(){
-    console.log("Task is done");
 
-    input_box = document.getElementById("input_box");
-    input_date = document.getElementById("input_date");
-}
-
-function create_unfinished_task(){
-}
-
-function create_finished_task() {
-}
